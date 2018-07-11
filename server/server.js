54689561d3b8ac31887beeea6a3919e2fe9b3f72
ssdorwat <ssdorwat@ncsu.edu>
2018-07-11 00:18:27 -0400

    sockets.io added for server as well as client

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const path = require('path')
 const express = require('express');
+const socketIO = require('socket.io')
+const http = require('http')
+
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 
 app.use(express.static(publicPath));
-// app.get('/', ( req, res) => {
-//     res.render('',publicPath+'/index.html')
-// })
 
+io.on('connection', (socket) => {
+    console.log(socket);
+    console.log('starting new connection')
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+    })
+})
+
+server.listen(port, () => {
     console.log(`server is runnig on post ${port}`);
 })
 
